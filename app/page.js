83de8fb4b7b5648f757217131e62a7eeb2071b5a
yoshinoa <alexander.yoshino@mail.utoronto.ca>
2023-05-28 00:21:04 -0400

    'pog'

--- app/page.js
+++ app/page.js
@@ -6,10 +6,16 @@ function ChampionGenerator() {
   const [blueTeam, setBlueTeam] = useState([]);
   const [redTeam, setRedTeam] = useState([]);
 
+  const [selectedLanguage, setSelectedLanguage] = useState("en_US");
+
+  const handleChangeLanguage = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
+
   const generateChampions = async () => {
     try {
       const response = await axios.get(
-        "https://ddragon.leagueoflegends.com/cdn/11.11.1/data/zh_CN/champion.json"
+        `https://ddragon.leagueoflegends.com/cdn/11.11.1/data/${selectedLanguage}/champion.json`
       );
       const championsData = response.data.data;
       const champions = Object.values(championsData).map(
@@ -31,6 +37,20 @@ function ChampionGenerator() {
   return (
     <div>
       <h1>League of Legends Champion Generator</h1>
+      <div>
+        <label htmlFor="language">Select Language:</label>
+        <select
+          id="language"
+          value={selectedLanguage}
+          onChange={handleChangeLanguage}
+        >
+          <option value="en_US">English</option>
+          <option value="zh_CN">中文</option>
+          <option value="ja_JP">日本語</option>
+          <option value="fr_FR">French</option>
+          {/* Add more language options as needed */}
+        </select>
+      </div>
       <button onClick={generateChampions}>Generate Champions</button>
       {blueTeam.length > 0 && redTeam.length > 0 && (
         <div>
