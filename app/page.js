62e197442de487e2938374e0c02bd0dccd3ce5a1
yoshinoa <alexander.yoshino@mail.utoronto.ca>
2023-05-27 22:52:25 -0400

    'initial'

--- app/page.js
+++ app/page.js
@@ -1,95 +1,55 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function Home() {
+function ChampionGenerator() {
+  const [blueTeam, setBlueTeam] = useState([]);
+  const [redTeam, setRedTeam] = useState([]);
+
+  const generateChampions = async () => {
+    try {
+      const response = await axios.get(
+        "https://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json"
+      );
+      const championsData = response.data.data;
+      const champions = Object.values(championsData).map(
+        (champion) => champion.name
+      );
+
+      const shuffledChampions = champions.sort(() => 0.5 - Math.random());
+
+      const blueTeamChampions = shuffledChampions.slice(0, 15);
+      const redTeamChampions = shuffledChampions.slice(15, 30);
+
+      setBlueTeam(blueTeamChampions);
+      setRedTeam(redTeamChampions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+    <div>
+      <h1>League of Legends Champion Generator</h1>
+      <button onClick={generateChampions}>Generate Champions</button>
+      {blueTeam.length > 0 && redTeam.length > 0 && (
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h2>Blue Team:</h2>
+          <ul>
+            {blueTeam.map((champion, index) => (
+              <li key={index}>{champion}</li>
+            ))}
+          </ul>
+          <h2>Red Team:</h2>
+          <ul>
+            {redTeam.map((champion, index) => (
+              <li key={index}>{champion}</li>
+            ))}
+          </ul>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      )}
+    </div>
+  );
 }
+
+export default ChampionGenerator;
